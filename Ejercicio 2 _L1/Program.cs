f81d7f7b9f596618cc088332abab73109adc236a
alexi <alexi@ALEXANDER>
2023-08-27 14:10:51 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio 2 _L1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Ingrese los minutos");
+int totalminutos = int.Parse(Console.ReadLine());
+
+int horas = totalminutos / 60;
+int minutos = totalminutos % 60;
+
+Console.WriteLine(horas + " horas y " + minutos + " minutos");
